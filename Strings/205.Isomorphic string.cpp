0900dab948509f045c9956023c78660238de3df7
Sneha Thakur <197256556+SnehaThakur19@users.noreply.github.com>
2026-06-11 17:38:35 +0530

    Solved:Isomorphic String

---
+++ Strings/205.Isomorphic string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        map<char, char> m1, m2;
+        for (int i = 0; i < s.length(); i++) {
+            if (m1.find(s[i]) == m1.end() && m2.find(t[i]) == m2.end()) {
+                m1[s[i]] = t[i];
+                m2[t[i]] = s[i];
+            }
+            if (m1[s[i]] != t[i] || m2[t[i]] != s[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
