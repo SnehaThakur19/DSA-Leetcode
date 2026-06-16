3330dc76e9774637b9d8d5a1c22d1f54d31c8d31
Sneha Thakur <197256556+SnehaThakur19@users.noreply.github.com>
2026-06-16 20:08:28 +0530

    Solved : Delete tail of LL

---
+++ Linked List/Delete tail of LL.cpp
@@ -0,0 +1,10 @@
+Node* removeTail(Node* head) {
+    if (head == NULL || head->next == NULL)   return NULL; 
+    Node* temp = head; 
+    while (temp->next->next != NULL) {
+        temp = temp->next; 
+    }
+    delete temp->next; 
+    temp->next = nullptr;
+    return head; 
+}
